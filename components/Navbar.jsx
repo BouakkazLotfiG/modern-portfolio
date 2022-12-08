95800a64478c7977a9ceedeb91e8519ad28d4f78
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-12-08 21:03:50 +0000

    mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import '../styles/Navbar.scss';
+import { socials } from '../constants';
 
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
@@ -24,36 +25,50 @@ const Navbar = () => {
             BOUAKKAZ
           </h2>
 
-          <img src='/menu.svg' alt='menu2' onClick={() => setToggle(true)} />
-        </div>
-      </motion.nav>
-      <div className='app__navbar-menu '>
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
+          {toggle ? (
             <img
               src='/arrow.svg'
-              alt='menu'
+              alt='menu2'
               onClick={() => {
                 setToggle(false);
                 console.log(toggle);
               }}
             />
-            <ul>
-              {['home', 'about', 'portfolio', 'skills', 'contact'].map(
-                (item) => (
-                  <li key={item}>
-                    <a href={`#${item}`} onClick={() => setToggle(false)}>
-                      {item}
-                    </a>
-                  </li>
-                )
-              )}
-            </ul>
-            <div className='mb-[50px] h-[2px] bg-white opacity-10 z-40' />
-            <a href='mailto:bouakkaz144.lotfi@gmail.com'>
+          ) : (
+            <img
+              src='/menu.svg'
+              alt='menu2'
+              className='close'
+              onClick={() => {
+                setToggle(true);
+                console.log(toggle);
+              }}
+            />
+          )}
+        </div>
+      </motion.nav>
+
+      {toggle && (
+        <motion.div
+          whileInView={{ x: [300, 0] }}
+          transition={{ duration: 0.85, ease: 'easeOut' }}
+          className='menu backdrop-blur-xl '
+        >
+          <ul>
+            {['Home', 'About', 'Portfolio', 'Skills', 'Contact'].map((item) => (
+              <li key={item} className='text-secondary-white'>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className='menu-bottom'>
+            <div className=' h-[2px] bg-white opacity-10 z-40' />
+            <a
+              href='mailto:bouakkaz144.lotfi@gmail.com'
+              className='contact-button'
+            >
               <button
                 type='button'
                 className='flex items-center h-fit py-3 px-4 bg-[#25618B] rounded-[32px] gap-[12px]'
@@ -68,9 +83,29 @@ const Navbar = () => {
                 </span>
               </button>
             </a>
-          </motion.div>
-        )}
-      </div>
+            <div className='socials'>
+              {socials.map((social) => (
+                <a
+                  key={social}
+                  href={social.url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <img
+                    key={social.name}
+                    src={social.icon}
+                    alt={social.name}
+                    className='w-[28px] h-[28px] object-contain cursor-pointer hover:scale-120 '
+                  />
+                </a>
+              ))}
+            </div>
+            <p className='font-normal text-[14px] text-white opacity-50'>
+              Made by Lotfi.
+            </p>
+          </div>
+        </motion.div>
+      )}
     </>
   );
 };
